a67aea417df1fe3c1790c5fbb0f2cd83797fa2e9
matsuyoshi <sfbgwm30@gmail.com>
2020-05-24 00:03:55 +0900

    Use urfave/cli package

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+const name = "gournal"
+
 func main() {
-	print("hello")
+	os.Exit(run(os.Args))
+}
+
+const (
+	exitOK = iota
+	exitNG
+)
+
+func run(args []string) int {
+	app := &cli.App{
+		Name:    name,
+		Usage:   "Journal tool written in Go",
+		Version: "0.0.1",
+	}
+
+	if err := app.Run(args); err != nil {
+		fmt.Println(err)
+		return exitNG
+	}
+	return exitOK
 }
